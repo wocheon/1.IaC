20feadae6c55e80766a2f4ffa544d2a4be4909b8
wocheon <82438284+wocheon@users.noreply.github.com>
2023-08-07 16:21:08 +0900

    Update calico.sh

--- K8S/calico.sh
+++ K8S/calico.sh
@@ -3,4 +3,6 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 export KUBECONFIG=/etc/kubernetes/admin.conf
 #kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/tigera-operator.yaml
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/tigera-operator.yaml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+systemctl restart kubelet
