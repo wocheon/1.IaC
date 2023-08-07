bbc21ff58dfa19a619ea3b18a31e1d39c22c297d
wocheon <wocheon07@gmail.com>
2023-08-07 17:45:17 +0900

    202308071745

---
+++ K8S/Default_setting.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo -i << EOF
+echo "root:welcome2" | /sbin/chpasswd
+echo "wocheon07:welcome2" | /sbin/chpasswd
+sed -i 's/=enforcing/=disabled/g' /etc/selinux/config ; setenforce 0
+systemctl disable firewalld --now
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+yum install -y git curl wget ansible
+echo "$(hostname -i) $(hostname)" >> /etc/hosts
+EOF
