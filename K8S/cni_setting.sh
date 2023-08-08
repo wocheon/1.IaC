1e935a8a6195d5898ed983550864f7459a098751
wocheon <wocheon07@gmail.com>
2023-08-08 16:39:50 +0900

    202308081639

--- K8S/cni_setting.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p $HOME/.kube
-cp -ri /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-export KUBECONFIG=/etc/kubernetes/admin.conf
-#kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
-#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/tigera-operator.yaml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-systemctl restart kubelet
--- K8S/kubeadm_reset.sh
+++
@@ -1,5 +0,0 @@
-kubeadm reset 
-rm -rf /etc/cni/net.d
-ipvsadm --clear
-rm -rf $HOME/.kube/config
-rm -rf /run/flannel/subnet.env
