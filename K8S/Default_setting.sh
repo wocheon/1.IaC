6c663a0964756a0daa48b278577e64ee12b1df72
wocheon <ciw0707@naver.com>
2023-08-08 02:02:49 +0000

    202308080202

--- K8S/Default_setting.sh
+++ K8S/Default_setting.sh
@@ -7,6 +7,6 @@ systemctl disable firewalld --now
 sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
-yum install -y git curl wget ansible
+yum install -y git curl wget ansible bash-completion
 echo "$(hostname -i) $(hostname)" >> /etc/hosts
 EOF
