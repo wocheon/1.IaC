51f07a22aff97ae8549ba81e2f0b017bef168914
wocheon <wocheon07@gmail.com>
2023-08-08 16:48:36 +0900

    202308081648

---
+++ K8S/ubuntu_20.04_LTS/1.Default_setting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo -i << EOF
+echo "root:welcome1" | /usr/sbin/chpasswd
+echo "wocheon07:welcome1" | /usr/sbin/chpasswd
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab ; swapoff -a ;
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+apt-get install -y git curl wget ansible bash-completion
+echo "$(hostname -i) $(hostname)" >> /etc/hosts
+EOF
